9631101b7a8c29e568b4d3f1f6e2ba20bc35b704
Josip Lovrečić <josip.lovrecic@hotmail.com>
2023-02-27 00:48:59 +0100

    UI update

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -10,9 +10,9 @@ const Chat = ({ messages, currentMember, onSendMessage, members }) => {
     console.log(members);
 
     return <>
-        <div className="container">
-            <div className="row clearfix">
-                <div className="col-lg-12">
+        <div className="container h-100">
+            <div className="row h-100">
+                <div className="col-lg-12 h-100">
                     <div className="card chat-app">
                         <div id="plist" className="people-list">
                             <ul className="list-unstyled chat-list mt-2 mb-0">
@@ -23,7 +23,7 @@ const Chat = ({ messages, currentMember, onSendMessage, members }) => {
                                         <img src={`https://api.dicebear.com/5.x/avataaars/svg?seed=${people.clientData.username}`} alt={`${people.clientData.username} avatar`} />
                                         <div className="about">
                                             <div className="name">{people.clientData.username}</div>
-                                            <div className="status"> <i className="fa fa-circle online"></i> {people.status === 'online' ? 'Online' : 'Offline'} </div>
+                                            {/* <div className="status"> <i className="fa fa-circle online"></i> {people.clientData.isOnline === 'online' ? 'Online' : 'Offline'} </div> */}
                                         </div>
                                     </li>
                                 ))
@@ -36,19 +36,18 @@ const Chat = ({ messages, currentMember, onSendMessage, members }) => {
                                     <div className="col-lg-6">
                                         <a href="#" data-toggle="modal" data-target="#view_info">
                                             <img src={`https://api.dicebear.com/5.x/avataaars/svg?seed=${currentMember.username}`} alt={`${currentMember.username} avatar`} />
-
                                         </a>
                                         <div className="chat-about">
                                             <h6 className="m-b-0">{currentMember.username}</h6>
-                                            <small>Last seen: 2 hours ago</small>
+                                            {/* <small>Last seen: 2 hours ago</small> */}
                                         </div>
                                     </div>
                                     <div class="col-lg-6 hidden-sm text-right">
-                                        <a href="#" className="btn btn-outline-warning"><i className="fa fa-question"></i></a>
+                                        {/* <a href="#" className="btn btn-outline-warning"><i className="fa fa-question"></i></a> */}
                                     </div>
                                 </div>
                             </div>
-                            <div className="chat-history">
+                            <div className="chat-history h-100 overflow-auto">
                                 <Messages messages={messages} member={currentMember} />
                             </div>
                             <div className="chat-message clearfix">
