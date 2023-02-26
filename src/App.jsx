c131b3f3d692c9764a4f5ce6c894d7e9538cdeac
Josip Lovrečić <josip.lovrecic@hotmail.com>
2023-02-26 22:17:20 +0100

    Login UI update

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,95 @@
 import React, { Component } from 'react';
 import Chat from "./Chat/Chat";
-import { randomColor, randomName } from './Utility/Utility';
+import Login from './Login/Login';
+import { randomColor } from './Utility/Utility';
 
 class App extends Component {
   state = {
     messages: [],
     members: [],
     member: {
-      username: randomName(),
+      username: "",
       color: randomColor(),
     },
-  }
+    connected: false,
+  };
 
-  drone = new window.Scaledrone("tHjsnInOpXhV11mZ", {
-    data: this.state.member
-  });
+  drone = null;
 
   componentDidMount() {
-    this.drone.on('open', error => {
-      if (error) {
-        return console.error(error);
-      }
-      const member = {...this.state.member};
-      member.id = this.drone.clientId;
-      this.setState({member});
+    // Drone initialization will be done after the user enters a username
+  }
+
+  handleLogin = (username) => {
+    // Initialize the drone object with the new username
+    const member = { ...this.state.member, username };
+    this.drone = new window.Scaledrone("tHjsnInOpXhV11mZ", {
+      data: member,
     });
 
+    // Set the drone.on('open') handler
+    this.drone.on("open", () => {
+      const member = { ...this.state.member, username };
+      member.id = this.drone.clientId;
+      this.setState({ member, connected: true });
+    });
+
+    // Subscribe to the room and handle events
     const room = this.drone.subscribe("observable-room");
 
-    room.on('data', (data, member) => {
+    room.on("data", (data, member) => {
       const messages = this.state.messages;
       const timestamp = new Date().toISOString();
-      messages.push({member, text: data, timestamp});
-      this.setState({messages});
+      messages.push({ member, text: data, timestamp });
+      this.setState({ messages });
     });
 
-    room.on('members', members => {
+    room.on("members", (members) => {
       this.setState({ members });
     });
-  
-    room.on('member_join', member => {
+
+    room.on("member_join", (member) => {
       const members = [...this.state.members];
 
-      if (!members.find(m => m.id === member.id)) {
+      if (!members.find((m) => m.id === member.id)) {
         members.push(member);
         this.setState({ members });
       }
     });
-  
-    room.on('member_leave', ({ id }) => {
-      const members = this.state.members.filter(member => member.id !== id);
+
+    room.on("member_leave", ({ id }) => {
+      const members = this.state.members.filter((member) => member.id !== id);
       this.setState({ members });
     });
-  }
+
+    // Connect to the drone server
+    this.drone.connect();
+  };
 
   render() {
     return (
       <div className="App">
-        <Chat
-        messages={this.state.messages}
-        currentMember={this.state.member}
-        onSendMessage={this.onSendMessage}
-        members={this.state.members}
-        />
+        {!this.state.connected && (
+          <Login onLogin={this.handleLogin} />
+        )}
+        {this.state.connected && (
+          <Chat
+            messages={this.state.messages}
+            currentMember={this.state.member}
+            onSendMessage={this.onSendMessage}
+            members={this.state.members}
+          />
+        )}
       </div>
-      
     );
   }
 
-  onSendMessage = message => {
+  onSendMessage = (message) => {
     this.drone.publish({
-      room: 'observable-room',
+      room: "observable-room",
       message,
     });
   };
-
 }
 
 export default App;
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,72 +1,62 @@
-import { useState } from "react";
-import { randomName } from "../Utility/Utility";
+import React, { Component } from 'react';
+import { randomName } from '../Utility/Utility';
 
-function Login({onLogin, onUsernameChange}) {
-    const [username, setUsername] = useState("");
-  
-    const handleLogin = () => {
-      onLogin(username);
-    };
-  
-    const handleUsernameChange = (newUsername) => {
-      setUsername(newUsername);
-      onUsernameChange(newUsername);
-    };
-  
+class Login extends Component {
+  state = {
+    username: "",
+  };
+
+  handleRandomNameClick = () => {
+    const randomUsername = randomName();
+    this.setState({ username: randomUsername });
+  };
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onLogin(this.state.username);
+  };
+
+  handleChange = (event) => {
+    this.setState({ username: event.target.value });
+  };
+
+  render() {
     return (
-      <div className="login-container">
-        <h2>Login</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleLogin();
-          }}
-        >
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => handleUsernameChange(e.target.value)}
-          />
-          <button type="submit">Log in</button>
-          <button
-            type="submit"
-            onClick={() => setUsername(randomName())}
-          >
-            Generate random username
-          </button>
-        </form>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className='container'>
+          <div className='col-md-6 offset-md-3 col-12'>
+            <div className='cardLogin'>
+              <div className='row mb-3'>
+                <div className='col-12'>
+                  <h3>Login</h3>
+                </div>
+              </div>
+              <div className='row mb-3'>
+                <div className="form-floating mb-3">
+                  <input 
+                    type="username" 
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                    className="form-control"
+                    id="floatingInput"/>
+                  <label for="floatingInput">Please enter username</label>
+                </div>
+              </div>
+              <div className='row'>
+                    <div className='col-6 mb-3'>
+                      <button type="button" className="btn btn-outline-secondary loginButton" onClick={this.handleRandomNameClick}>Random username</button>
+                    </div>
+                    <div className='col-6 mb-3'>
+                      <button type="submit" className="btn btn-outline-success loginButton">Login</button>
+                    </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
-  
+}
 
-// function Login({onLogin}) {
-//     const [username, setUsername] = useState('');
-  
-//     const handleLogin = () => {
-//         onLogin(username);
-//     };
-  
-//     return (
-//       <div className="login-container">
-//         <h2>Login</h2>
-//         <form onSubmit={e => { e.preventDefault(); handleLogin(); }}>
-//           <label htmlFor="username">Username:</label>
-//           <input
-//             type="text"
-//             id="username"
-//             value={username}
-//             onChange={e => setUsername(e.target.value)}
-//           />
-//           <button type="submit">Log in</button>
-//           <button type="submit" onClick={() => setUsername(randomName())}>
-//             Generate random username
-//           </button>
-//         </form>
-//       </div>
-//     );
-//   }
-  
-  export default Login;
+export default Login;
